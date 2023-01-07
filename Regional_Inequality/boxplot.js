5fd26e5b5da03e9b0724bdfcf30d47f6cbb7a8dd
FM-ds <72876918+FM-ds@users.noreply.github.com>
2023-01-07 23:11:12 +0000

    Using NUTS2 not NUTS3

--- Regional_Inequality/boxplot.js
+++ Regional_Inequality/boxplot.js
@@ -11,9 +11,8 @@ let ax_weight = "0.25px";
 let countries = ['United Kingdom', 'France', 'Germany', 'Netherlands', 'Denmark'];
 
 let quantiles_unweighted = {"Germany": {"q1": 0.8357348703170029, "q2": 1.0, "q3": 1.207492795389049}, "Denmark": {"q1": 0.8851351351351351, "q2": 1.0, "q3": 1.170045045045045}, "Spain": {"q1": 0.9140969162995595, "q2": 1.0, "q3": 1.2422907488986783}, "France": {"q1": 0.9330855018587361, "q2": 1.0, "q3": 1.1561338289962826}, "Italy": {"q1": 0.75, "q2": 1.0, "q3": 1.2055555555555555}, "Netherlands": {"q1": 0.9056356487549148, "q2": 1.0, "q3": 1.1448230668414154}, "Poland": {"q1": 0.8691588785046729, "q2": 1.0, "q3": 1.1869158878504673}, "United Kingdom": {"q1": 0.8297737408453009, "q2": 1.0, "q3": 1.2350763062067838}}
-
-let quantiles = {"Denmark": {"q1": 0.8988690497909927, "q2": 1.0, "q3": 1.13170729487961}, "United Kingdom": {"q1": 0.8311065295003994, "q2": 1.0, "q3": 1.269002889661404}, "Germany": {"q1": 0.8243325937477065, "q2": 1.0, "q3": 1.2231540469082363}, "Spain": {"q1": 0.867438950828485, "q2": 0.9999999999999999, "q3": 1.3005108376290155}, "France": {"q1": 0.8590398412733005, "q2": 1.0, "q3": 1.1848432276233725}, "Italy": {"q1": 0.6560532777725007, "q2": 1.0, "q3": 1.137537424904338}, "Netherlands": {"q1": 0.8913022005356929, "q2": 1.0, "q3": 1.1697285766015968}, "Poland": {"q1": 0.8793245800999343, "q2": 1.0, "q3": 1.2534860963329633}}
-
+let quantiles_nuts3 = {"Denmark": {"q1": 0.8988690497909927, "q2": 1.0, "q3": 1.13170729487961}, "United Kingdom": {"q1": 0.8311065295003994, "q2": 1.0, "q3": 1.269002889661404}, "Germany": {"q1": 0.8243325937477065, "q2": 1.0, "q3": 1.2231540469082363}, "Spain": {"q1": 0.867438950828485, "q2": 0.9999999999999999, "q3": 1.3005108376290155}, "France": {"q1": 0.8590398412733005, "q2": 1.0, "q3": 1.1848432276233725}, "Italy": {"q1": 0.6560532777725007, "q2": 1.0, "q3": 1.137537424904338}, "Netherlands": {"q1": 0.8913022005356929, "q2": 1.0, "q3": 1.1697285766015968}, "Poland": {"q1": 0.8793245800999343, "q2": 1.0, "q3": 1.2534860963329633}}
+let quantiles = {"Denmark": {"q0": 0.7837115870276402, "q1": 0.9292826828363121, "q2": 1.0, "q3": 1.108973573827493, "q4": 1.4378495250000438}, "United Kingdom": {"q0": 0.7427471006375937, "q1": 0.8984282105613345, "q2": 1.0, "q3": 1.1867895123556627, "q4": 7.0833425837297534}, "Germany": {"q0": 0.7234930666776918, "q1": 0.8607400932574523, "q2": 1.0, "q3": 1.1226466890965092, "q4": 1.667502855733173}, "Spain": {"q0": 0.8114258649711136, "q1": 0.8877807712514793, "q2": 1.0, "q3": 1.288207453111353, "q4": 1.521423496820838}, "France": {"q0": 0.31004071867460153, "q1": 0.9221135537254428, "q2": 1.0, "q3": 1.0992283677237773, "q4": 1.96572208231835}, "Italy": {"q0": 0.538119021270963, "q1": 0.5949945638921474, "q2": 1.0, "q3": 1.0769938440042504, "q4": 1.4798273084951483}, "Netherlands": {"q0": 0.7268778255432972, "q1": 0.9000524799047096, "q2": 1.0000000000000002, "q3": 1.0793279807089393, "q4": 1.4264122846084766}, "Poland": {"q0": 0.7523232880599361, "q1": 0.8733495734086678, "q2": 1.0, "q3": 1.123267534993915, "q4": 2.4232729068035836}}
 //our x axis scale - not the axis itself
 let x_scale = d3.scalePoint()
     .domain(countries)
@@ -34,7 +33,7 @@ let secondStageScale = d3.scaleLog()
     );
 
 let sizeScale = d3.scaleLinear()
-    .domain([22025, 3644826])
+    .domain([84689.0,12252917.0])     //.domain([22025, 3644826])
     .range([1,13]) //TODO link to area not radius
 
 let countryScale = d3.scaleOrdinal(d3.schemeCategory10)
@@ -46,22 +45,42 @@ let foo = null
 
 box_data = null
 
-let plotArea, y, points_group, points_gs, points;
+let plotArea, y, points_group, points_gs, points, boxplotTooltip;
 
 // let median_Sections = ["DK013", "DE269", "ES416", "FRB01", "ITH56", "NL337", "PL618", "UKJ35"]
-let median_section_names = ["DK042", "DE128", "FR102", "NL221", "UKM92"]
+let median_section_names_nuts3 = ["DK042", "DE128", "FR102", "NL221", "UKM92"]
+let median_section_names = ['DK03', 'UKM92', 'DE22', 'ES11', 'FRI1', 'ITC3', 'NL11', 'PL21']
+
+
+// Tooltip Funcs
+let boxplotMouseover = function(d) {
+    console.log('mouseon');
+    // boxplotTooltip
+    //   .style("opacity", 1)
+    // d3.select(this)
+    //   .style("stroke", "black")
+    //   .style("opacity", 1)
+  }
+let boxplotMousemove = function(d) {
+    boxplotTooltip
+      .html("The exact value of<br>this cell is: ")
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+let boxplotMouseleave = function(d) {
+    boxplotTooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+  }
+
 
 d3.json(plotData, function (data) {
     box_data = data;
     //Defining the Plot Area
     plotArea = box_svg.append("g")
-        .attr("id", "plotArea")
-        .style("fill", "#bbdee2");
-    let plotBack = plotArea.append("rect")
-        .attr("width", 300)
-        .attr("height", 200)
-        .style("fill", "#ffffff")
-
+        .attr("id", "plotArea");
     foo = x_scale;
 
     y_scale = firstStageScale;
@@ -90,7 +109,10 @@ d3.json(plotData, function (data) {
             .style("fill", d => countryScale(d.Country))
             .style("stroke", "black")
             .style("stroke-width", 0.25)
-            .style("opacity", 0.7);
+            .style("opacity", 0.7)
+            .on("mouseover", boxplotMouseover)
+            .on("mousemove", boxplotMousemove)
+            .on("mouseleave", boxplotMouseleave);
 
     
     //Scale the plot area
@@ -107,6 +129,18 @@ d3.json(plotData, function (data) {
             }
             return(1)
         });
+
+    // Draw the tooltip 
+
+    boxplotTooltip = box_svg
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px")
 
 });
 
@@ -243,13 +277,42 @@ function drawBoxPlots(){
             - fill: countryScale(country)
         */
         console.log(country);
-        plotArea.append('rect')
+        plotArea.insert('rect', ":first-child")
             .attr("x", x_scale(country)-0.5*boxWidth)
             .attr("y", secondStageScale( quantiles[country].q3))
             .attr("height", secondStageScale( quantiles[country].q1 ) -  secondStageScale( quantiles[country].q3 ))
             .attr("width", boxWidth)
-            .style("fill", countryScale(country));
+            .style("stroke", countryScale(country))
+            .style("stroke-width", 0.5)
+            .style("fill", "rgb(200,200,200)");
+
+        // the upper whisker
+        plotArea.append('line')
+            .attr('x1', x_scale(country)-0.5*boxWidth)
+            .attr('x2', x_scale(country)+0.5*boxWidth)
+            .attr('y1', secondStageScale( quantiles[country].q0))
+            .attr('y2', secondStageScale( quantiles[country].q0))
+            .style("stroke", countryScale(country))
+            .style("stroke-width", 1)
+
+
+        // the upper whisker
+        plotArea.append('line')
+            .attr('x1', x_scale(country)-0.5*boxWidth)
+            .attr('x2', x_scale(country)+0.5*boxWidth)
+            .attr('y1', secondStageScale( quantiles[country].q4))
+            .attr('y2', secondStageScale( quantiles[country].q4))
+            .style("stroke", countryScale(country))
+            .style("stroke-width", 1);
+
+        //and the central lines
+        plotArea.insert('line', ":first-child")
+            .attr('x1', x_scale(country))
+            .attr('x2', x_scale(country))
+            .attr('y1', secondStageScale( quantiles[country].q0))
+            .attr('y2', secondStageScale( quantiles[country].q4))
+            .style("stroke", countryScale(country))
+            .style("stroke-width", 1);
 
     });
-
 }
